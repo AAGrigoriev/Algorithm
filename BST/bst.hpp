10cfc855e75bc73a61c6428fa2a57c0e88caef98
Andrei Grigoriev <grigorevaa@aigen.ru>
2022-03-26 21:36:08 +0300

    BST test

--- BST/bst.hpp
+++ BST/bst.hpp
@@ -9,31 +9,32 @@ namespace bst_tree {
 template<typename Key>
 struct node {
   Key val;
-  node* left;
-  node* right;
+  node* left = nullptr;
+  node* right = nullptr;
 };
 
 
 template <typename Key>
-class BST {
+class BST final {
 using bst_node = node<Key>;
 public:
   bst_node* insert(const Key& val);
   bst_node* remove(const Key& val);
-  bst_node* search(const Key& val);
-  // TODO destructor
+  bst_node* search(const Key& val) const;
+  ~BST();
 private:
   bst_node* root_ = nullptr;
 
-  bst_node* find_min_node(bst_node* root);
-  bst_node* insert_impl(bst_node* root, const Key& val);
+  void clear_tree(bst_node* root);
+  bst_node* find_min_node(bst_node* root) const;
+  bst_node* insert_impl(bst_node*& root, const Key& val);
   bst_node* remove_impl(bst_node* root, const Key& val);
-  bst_node* search_impl(bst_node* root, const Key& val);
+  bst_node* search_impl(bst_node* root, const Key& val) const;
 };
 
 
 template<typename Key>
-typename BST<Key>::bst_node* BST<Key>::find_min_node(bst_node* const root) {
+typename BST<Key>::bst_node* BST<Key>::find_min_node(bst_node* const root) const{
   auto min = root;
   while (min && min->left) {
     min = min->left;
@@ -44,20 +45,20 @@ typename BST<Key>::bst_node* BST<Key>::find_min_node(bst_node* const root) {
 
 template <typename Key>
 typename BST<Key>::bst_node* BST<Key>::insert(const Key& val) {
-  insert_impl(root_, val);
+  return insert_impl(root_, val);
 }
 
 
 template <typename Key>
-typename BST<Key>::bst_node* BST<Key>::insert_impl(bst_node* root, const Key& val) {
+typename BST<Key>::bst_node* BST<Key>::insert_impl(bst_node*& root, const Key& val) {
   if (!root) {
     return root = new bst_node{val, nullptr, nullptr};
   }
 
   if (root->val > val) {
-    root->left = insert(root->left, val);
+    root->left = insert_impl(root->left, val);
   } else {
-    root->right = insert(root->right, val);
+    root->right = insert_impl(root->right, val);
   }
 
   return root;
@@ -66,7 +67,7 @@ typename BST<Key>::bst_node* BST<Key>::insert_impl(bst_node* root, const Key& va
 
 template <typename Key>
 typename BST<Key>::bst_node* BST<Key>::remove(const Key& val) {
-  return remove_impl(root_, val);
+  return root_ = remove_impl(root_, val);
 }
 
 
@@ -77,9 +78,9 @@ typename BST<Key>::bst_node* BST<Key>::remove_impl(bst_node* root, const Key& va
   }
 
   if (val < root->val) {
-    root->left = remove(root->left, val);
+    root->left = remove_impl(root->left, val);
   } else if (val > root->val) {
-    root->right = remove(root->right, val);
+    root->right = remove_impl(root->right, val);
   } else {
     if (!root->left && !root->right) {
       delete root;
@@ -96,29 +97,47 @@ typename BST<Key>::bst_node* BST<Key>::remove_impl(bst_node* root, const Key& va
 
     auto min_node = find_min_node(root);
     root->val = min_node->val;
-    root->right = remove(root->right, min_node->val);
+    root->right = remove_impl(root->right, min_node->val);
   }
   return root;
 }
 
 
 template <typename Key>
-typename BST<Key>::bst_node* BST<Key>::search(const Key& val) {
+typename BST<Key>::bst_node* BST<Key>::search(const Key& val) const {
   return search_impl(root_, val);
 }
 
 
 template <typename Key>
-typename BST<Key>::bst_node* BST<Key>::search_impl(bst_node* root, const Key& val) {
+typename BST<Key>::bst_node* BST<Key>::search_impl(bst_node* root, const Key& val) const {
   if (!root || root->val == val) {
     return root;
   }
 
   if (val > root->val) {
-    return search(root->right);
+    return search_impl(root->right, val);
   } else {
-    return search(root->left);
+    return search_impl(root->left, val);
   }
+}
+
+
+template <typename Key>
+void BST<Key>::clear_tree(bst_node* root) {
+  if (!root) {
+    return;
+  }
+
+  clear_tree(root->left);
+  clear_tree(root->right);
+  delete root;
+}
+
+
+template <typename Key>
+BST<Key>::~BST<Key>() {
+  clear_tree(root_);
 }
 
 }
--- BST/main.cpp
+++ BST/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include "bst.hpp"
 
 using namespace std;
 
-
-
-
-
 int main() {
-  cout << "Hello World!" << endl;
+  using namespace bst_tree;
+  BST<int> bst_tree;
+
+  for (std::size_t i = 0; i < 100; ++i) {
+    bst_tree.insert(i);
+  }
+
+  for (std::size_t i = 99; i > 50; --i) {
+    bst_tree.remove(i);
+  }
+
+  for (std::size_t i = 0; i <= 50; ++i) {
+    std::cout << bst_tree.search(i)->val << std::endl;
+  }
+
   return 0;
 }
